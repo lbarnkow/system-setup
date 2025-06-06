05204e8cf80017211a4d15150160511fb3420e8a
Lorenz Barnkow <13865903+lbarnkow@users.noreply.github.com>
2025-06-06 23:01:52 +0200

    wip

--- init.sh
+++ init.sh
@@ -8,9 +8,15 @@ echo ""
 echo "Cloning github.com/lbarnkow/system-setup repo..."
 read -p "Press enter to continue"
 git clone git@github.com:lbarnkow/system-setup.git ~/.system-setup 
+cd ~/.system-setup
 
-#echo "Creating python virtual env..."
-#python3 -m venv .venv
+echo ""
+echo "Creating python virtual env..."
+cd ansible
+python3 -m venv .venv
+. .venv/bin/activate
 
-
+echo ""
+echo "Installing python depedencies..."
+pip install -r requirements.txt
 
